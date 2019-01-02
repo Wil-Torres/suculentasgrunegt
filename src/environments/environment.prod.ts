8d90109a24ccd872b10caa8604bf70ba0e9bebff
Wtorres <wtorres@cads.com.gt>
2019-01-02 16:17:27 -0600

    Ajuste de conexion db

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: "AIzaSyAyriGurdQ48OMSb_QF4UI1tycnhJhf_SQ",
+    authDomain: "suculentasgrunegt.firebaseapp.com",
+    databaseURL: "https://suculentasgrunegt.firebaseio.com",
+    projectId: "suculentasgrunegt",
+    storageBucket: "suculentasgrunegt.appspot.com",
+    messagingSenderId: "542519528312"
+  }
 };
